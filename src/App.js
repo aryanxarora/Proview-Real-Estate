27651dc391bcbd9c8aaad68aa0bbf8609f3c7365
aryanxarora <aryanxarora@gmail.com>
2023-05-30 15:06:26 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav className="bg-red-500">
+      <div className="">
+        <h1>Hello</h1>
+        <img src="" alt="" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
