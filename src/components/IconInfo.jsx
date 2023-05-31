baf908fc3cb60b9ad3ad0459b7ad7606dcdeaffc
aryanxarora <aryanxarora@gmail.com>
2023-05-30 23:23:35 -0400

    Slideshow and Icons

---
+++ src/components/IconInfo.jsx
@@ -0,0 +1,29 @@
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import KingBedRoundedIcon from "@mui/icons-material/KingBedRounded";
+import ShowerRoundedIcon from "@mui/icons-material/ShowerRounded";
+import CalendarViewMonthRoundedIcon from "@mui/icons-material/CalendarViewMonthRounded";
+
+function IconInfo() {
+  return (
+    <div className="flex gap-10 md:w-full text-slate-500 animate-pulse">
+      <div>
+        <HomeRoundedIcon fontSize="medium" />
+        <p>7 Rooms</p>
+      </div>
+      <div>
+        <KingBedRoundedIcon />
+        <p>2 Bed(s)</p>
+      </div>
+      <div>
+        <ShowerRoundedIcon />
+        <p>2 Bath(s)</p>
+      </div>
+      <div>
+        <CalendarViewMonthRoundedIcon />
+        <p>828.28 sq.ft.</p>
+      </div>
+    </div>
+  );
+}
+
+export default IconInfo;
--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,22 +1,51 @@
+import { useState, useEffect } from "react";
+import IconInfo from "./IconInfo";
+
 function Listing() {
+  const slideshow = [
+    "url(https://www.macleweb.com/immobilier/large_photo/286172971.jpg)",
+    "url(https://www.macleweb.com/immobilier/large_photo/286172972.jpg)",
+    "url(https://www.macleweb.com/immobilier/large_photo/286172976.jpg)",
+    "url(https://www.macleweb.com/immobilier/large_photo/286172979.jpg)",
+  ];
+  const [bg, setBg] = useState(slideshow[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBg((prevValue) => {
+        const currentIndex = slideshow.indexOf(prevValue);
+        const nextIndex = (currentIndex + 1) % slideshow.length;
+        return slideshow[nextIndex];
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
+    <div className="md:flex xl:mx-20">
       <div
-        className="bg-cover bg-center bg-no-repeat bg-slate-200 p-5 m-5 rounded-2xl h-96 flex flex-col items-bottom justify-end pb-10"
+        className="h-96 md:h-auto bg-cover bg-center bg-no-repeat bg-slate-200 p-5 m-5 rounded-2xl flex flex-col items-bottom justify-end pb-10 md:basis-1/2 xl:mr-10"
         style={{
-          backgroundImage:
-            "url(https://www.macleweb.com/immobilier/large_photo/285025021.jpg)",
+          backgroundImage: bg,
         }}
       ></div>
-      <div className="mx-5">
-        <h1 className="text-2xl font-bold">242 Rue Young</h1>
-        <h2 className="text-sm mb-5">Montréal (Le Sud-Ouest)</h2>
-        <p className="text-md">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          culpa perspiciatis impedit quibusdam omnis et quisquam illum explicabo
-          eaque cum eos maiores pariatur saepe ullam, quae ipsa hic quo!
-          Tempore!
+      <div className="m-5 md:basis-1/2">
+        <h1 className="text-2xl md:text-3xl xl:text-4xl font-bold">
+          288 Rue Richmond
+        </h1>
+        <h2 className="text-sm md:text-base mb-5">Montréal (Le Sud-Ouest)</h2>
+        <p className="text-md md:text-xl">
+          Welcome to Quartier Général - Ultra chic luxury building located in
+          the heart of Griffintown. This brand new corner unit offers a bright
+          and spacious open concept living room and dining room with a modern
+          kitchen. The condo features 2 bedrooms and 2 bathrooms with a balcony
+          facing north/west with breathtaking city views. It also includes a
+          garage and storage locker. A must see!
         </p>
+        <div className="mt-10">
+          <IconInfo />
+        </div>
       </div>
     </div>
   );
