8d5450163680e240bf5f1c95658a1c83f0ec7490
aryanxarora <aryanxarora@gmail.com>
2024-01-02 00:28:13 -0500

    Landing Fixed

--- src/components/Landing/FeatureTile.jsx
+++ src/components/Landing/FeatureTile.jsx
@@ -10,8 +10,27 @@ function FeatureTile({ data }) {
         <p className="my-5 text-slate-500 font-sans text-[1rem] leading-relaxed lg:text-xl">
           {data.content}
         </p>
+        {data.features && (
+          <div>
+            {data.features.map((item) => (
+              <div>
+                <div className="flex py-3 gap-5 items-center">
+                  <div className="bg-gray-300 p-3 rounded">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/665/665939.png"
+                      alt="print"
+                      className="w-5"
+                    />
+                  </div>
+                  <p className="font-semibold text-gray-700">{item}</p>
+                </div>
+                {/* <hr /> */}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      <div className="lg:basis-1/2">
+      <div className="lg:basis-1/2 lg:p-10">
         <img src={data.image} alt="" />
       </div>
     </div>
--- src/models/features.js
+++ src/models/features.js
@@ -3,19 +3,23 @@ const features = [
     image:
       "https://raw.githubusercontent.com/aryanxarora/proview/main/src/images/feature1.png",
     title: "Leverage the power of video",
-    content: "Unlock the true potential of your properties with the power of video. Elevate your listings, captivate your audience, and make your real estate stand out in stunning, cinematic fashion.",
+    content:
+      "Unlock the true potential of your properties with the power of video. Elevate your listings, captivate your audience, and make your real estate stand out in stunning, cinematic fashion.",
   },
   {
     image:
       "https://raw.githubusercontent.com/aryanxarora/proview/main/src/images/feature2.png",
     title: "Automate your communication",
-    content: "Streamline your outreach effortlessly. Automate your email and SMS marketing for real estate to engage clients at the perfect moment. Save time, personalize messages, and stay connected.",
+    content:
+      "Streamline your outreach effortlessly. Automate your email and SMS marketing for real estate to engage clients at the perfect moment. Save time, personalize messages, and stay connected.",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+      "https://raw.githubusercontent.com/aryanxarora/proview/main/src/images/feature3.png",
     title: "All-in-one marketing solution",
-    content: 'Your one-stop marketing powerhouse. From digital campaigns to print materials, our all-in-one solution takes care of your entire real estate marketing strategy.',
+    content:
+      "Your one-stop marketing powerhouse. From digital campaigns to print materials, our all-in-one solution takes care of your entire real estate marketing strategy.",
+    features: ["Print Advertising", "Virtual Tours", "Dedicated Support"],
   },
 ];
 
